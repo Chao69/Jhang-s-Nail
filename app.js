1befeedab8222a72eb5520824f12b4b3371267e6
Chao <81567144+Chao69@users.noreply.github.com>
2021-11-22 05:51:23 +0800

    add express-session

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const session = require('express-session')
 
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.use(session({
+  secret: 'jhang',
+  resave: false,
+  saveUninitialized: true
+}))
 app.get('/', (req, res) => {
   res.render('home')
 })
